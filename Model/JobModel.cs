aee697b5710172e6f7b70c4cb9ce16507ec749ce
PunkSamurai <dveliath@gmail.com>
2025-01-28 13:25:57 +0530

    Revert "figured out the piechart somewhat"
    This reverts commit b1f31229ce2e10c526df61286d61b85d69eca0f3.

--- Model/JobModel.cs
+++ Model/JobModel.cs
@@ -1,6 +1,4 @@
-﻿using OxyPlot.Series;
-using OxyPlot;
-using System;
+﻿using System;
 
 namespace JobReporter2.Model
 {
@@ -21,81 +19,17 @@ namespace JobReporter2.Model
         public TimeSpan TotalTime { get; set; }
         public TimeSpan? CutTime { get; set; }
 
-        public TimeSpan MachineTime { get; set; }
-
-        public double? FeedrateOverride { get; set; }
+        public float? FeedrateOveride { get; set; }
 
         public TimeSpan? SlewTime { get; set; }
         public TimeSpan? PauseTime { get; set; }
         public float? SheetCount { get; set; }
-
-        public int? PD_Count { get; set; }
-        public string Material { get; set; }
         public TimeSpan? SheetChangeTime { get; set; }
         public string Tools { get; set; }
         public TimeSpan? ToolChangeTime { get; set; }
         public TimeSpan? ToolAvgTimes { get; set; }
 
-        public string Size { get; set; }
         public bool Flagged { get; set; }
 
-
-        public PlotModel PieChartModel { get; private set; }
-
-        public JobModel()
-        {
-            GeneratePieChart();
-        }
-
-        private double TimeSpanToValue(TimeSpan? timeSpan) => timeSpan?.TotalSeconds ?? 0;
-
-        private string FormatSliceLabel(string name, TimeSpan? time)
-        {
-            if (time == null || time.Value.TotalSeconds == 0)
-                return $"{name}: 0s";
-
-            var ts = time.Value;
-            return ts.TotalMinutes >= 1
-                ? $"{name}: {ts.Minutes}m {ts.Seconds}s"
-                : $"{name}: {ts.Seconds}s";
-        }
-
-        public void GeneratePieChart()
-        {
-            var plotModel = new PlotModel { Title = "Time Distribution" };
-            var pieSeries = new PieSeries
-            {
-                StrokeThickness = 2.0,
-                InsideLabelPosition = 0.8,
-                AngleSpan = 360,
-                StartAngle = 0,
-                FontSize = 12
-            };
-
-            var timeData = new[]
-            {
-                (Name: "Cut", Time: CutTime, Color: OxyColors.Green),
-                (Name: "Slew", Time: SlewTime, Color: OxyColors.Blue),
-                (Name: "Pause", Time: PauseTime, Color: OxyColors.Red),
-                (Name: "Sheet Change", Time: SheetChangeTime, Color: OxyColors.Yellow),
-                (Name: "Tool Change", Time: ToolChangeTime, Color: OxyColors.Purple)
-            };
-
-            foreach (var data in timeData)
-            {
-                pieSeries.Slices.Add(new PieSlice(
-                    FormatSliceLabel(data.Name, data.Time),
-                    TimeSpanToValue(data.Time))
-                {
-                    Fill = data.Color,
-                    IsExploded = true
-                });
-            }
-
-            plotModel.Series.Add(pieSeries);
-            PieChartModel = plotModel;
-            Console.WriteLine(CutTime?.TotalSeconds);
-            Console.WriteLine("plotted");
-        }
     }
 }
--- View/JobsContent.xaml.cs
+++ View/JobsContent.xaml.cs
@@ -1,5 +1,4 @@
-﻿using JobReporter2.ViewModel;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,7 +23,6 @@ namespace JobReporter2.View
         public JobsContent()
         {
             InitializeComponent();
-            DataContext = new MainViewModel();
         }
     }
 }
